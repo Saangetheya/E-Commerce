4c4ee2ac5e316204990213db6368c625e87b0d05
Abhijith Shaji <88903559+Saangetheya@users.noreply.github.com>
2023-05-08 04:41:32 +0530

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,90 +1,13 @@
 import React from "react";
-import {
-  Home,
-  WishList,
-  ProtectedRoute,
-  AdminProtectedRoute,
-  CartProtectedRoute,
-  PageNotFound,
-  ProductDetails,
-  ProductByCategory,
-  CheckoutPage,
-} from "./shop";
-import { DashboardAdmin, Categories, Products, Orders } from "./admin";
-import { UserProfile, UserOrders, SettingUser } from "./shop/dashboardUser";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Blog from "./shop/blog/blog";
-import Contactus from "./shop/contact-us/Contactus";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-const Routes = (props) => {
-  return (
-    <Router>
-      <Switch>
-        {/* Shop & Public Routes */}
-        <Route exact path="/" component={Home} />
-        <Route exact path="/wish-list" component={WishList} />
-        <Route exact path="/products/:id" component={ProductDetails} />
-        <Route exact path="/blog" component={Blog} />
-        <Route exact path="/contact-us" component={Contactus} />
-        <Route
-          exact
-          path="/products/category/:catId"
-          component={ProductByCategory}
-        />
-        <CartProtectedRoute
-          exact={true}
-          path="/checkout"
-          component={CheckoutPage}
-        />
-        {/* Shop & Public Routes End */}
-
-        {/* Admin Routes */}
-        <AdminProtectedRoute
-          exact={true}
-          path="/admin/dashboard"
-          component={DashboardAdmin}
-        />
-        <AdminProtectedRoute
-          exact={true}
-          path="/admin/dashboard/categories"
-          component={Categories}
-        />
-        <AdminProtectedRoute
-          exact={true}
-          path="/admin/dashboard/products"
-          component={Products}
-        />
-        <AdminProtectedRoute
-          exact={true}
-          path="/admin/dashboard/orders"
-          component={Orders}
-        />
-        {/* Admin Routes End */}
-
-        {/* User Dashboard */}
-        <ProtectedRoute
-          exact={true}
-          path="/user/profile"
-          component={UserProfile}
-        />
-        <ProtectedRoute
-          exact={true}
-          path="/user/orders"
-          component={UserOrders}
-        />
-        <ProtectedRoute
-          exact={true}
-          path="/user/setting"
-          component={SettingUser}
-        />
-        {/* User Dashboard End */}
-
-        {/* 404 Page */}
-        <Route component={PageNotFound} />
-      </Switch>
-    </Router>
-  );
-};
-
-export default Routes;
+serviceWorker.unregister();
